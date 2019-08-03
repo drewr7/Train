b4764d6b7c579a7882ac4e05f8f871f4a7dfa4f6
Drew Reynolds <drew@sportscrm.com>
2019-08-02 23:28:06 -0500

    data is being pushed to firebase but not returning

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD8_oeqODULT60elD4Aii618XDGWrqIVMQ",
-    authDomain: "timesheet-app-18829.firebaseapp.com",
-    databaseURL: "https://timesheet-app-18829.firebaseio.com",
-     projectId: "timesheet-app-18829",
-     storageBucket: "",
-    messagingSenderId: "860258239492",
-    appId: "1:860258239492:web:27182905d181a39e"
+  apiKey: "AIzaSyBD7TCg1YOjfjuceZcPoMO9xhQBgFpyJuk",
+  authDomain: "train-4349e.firebaseapp.com",
+  databaseURL: "https://train-4349e.firebaseio.com",
+  projectId: "train-4349e",
+  storageBucket: "",
+  messagingSenderId: "967617258883",
+  appId: "1:967617258883:web:932464e7d225b0ab"
   };
 
   firebase.initializeApp(config);
@@ -14,63 +14,61 @@ var config = {
   // Create a variable to reference the database.
   var dataRef = firebase.database();
 
-  // Initial Values
-  var name = "";
-  var role = "";
-  var startDate = "";
-  var monthlyRate = 0;
-  var monthsWorked = 0;
-  var totalBilled = 0;
-
   // Capture Button Click
-  $("#add-emp").on("click", function(event) {
+  $("#add-train").on("click", function(event) {
     event.preventDefault();
     console.log(event);
 
     // Grabbed values from text boxes
-    name = $("#emp_name").val().trim();
-    role = $("#role").val().trim();
-    startDate = moment(
-                        $("#startDate")
-                        .val()
-                        .trim(),
-                        "YYYY-MM-DD"
-                        ).format("X");
-    monthlyRate = $("#rate").val().trim();
-console.log($("#startDate").val());
+    var name = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var tFrequency = $("#frequency").val().trim();
+    var firstTime = $("#first-train").val().trim();
 
+    
     // Code for handling the push
     dataRef.ref().push({
       name: name,
-      role: role,
-      startDate: startDate,
-      monthlyRate: monthlyRate,
+      destination: destination,
+      tFrequency: tFrequency,
+      firstTime: firstTime,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#frequency").val("");
+    $("#first-train").val("");
 
   });
 
   dataRef.ref().on("child_added", function(childSnapshot) {
 
-    // Log everything that's coming out of snapshot
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().role);
-    console.log(childSnapshot.val().startDate);
-    console.log(childSnapshot.val().monthlyRate);
-    console.log(childSnapshot.val().dateAdded);
-   
-    var monthsWorked = moment().diff(startDate, 'months')
-    console.log (monthsWorked);
-    var totalBilled
-
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     // full list of items to the well
-    $("#curr-emp").append("<tr> " +
+    $("#curr-trains").append("<tr> " +
       " <th scope='row'> " + childSnapshot.val().name + 
-      " </th><td id='tbl_role'> " + childSnapshot.val().role +
-      " </td><td id='tbl_startDate'> " + moment.unix(childSnapshot.val().startDate).format("MM/DD/YYYY") +
-      " </td><td id='tbl_months-worked'> " + childSnapshot.val().monthsWorked +
-      " </td><td id='tbl_monthly_rate'> " + childSnapshot.val().monthlyRate +
+      " </th><td id='tbl_destination'> " + childSnapshot.val().destination +
+      " </td><td id='tbl_frequency'> " + childSnapshot.val().tFrequency+
+      " </td><td id='tbl_next-arrival'> " + childSnapshot.val().nextTrain +
+      " </td><td id='tbl_min-away'> " + childSnapshot.val().tMinutesTillTrain +
       " </td><td id='tbl_total_billed> " + childSnapshot.val().totalBilled +
       //" </td><td id='tbl_total_billed> " + childSnapshot.val().totalBilled +
       " </td></tr>");
